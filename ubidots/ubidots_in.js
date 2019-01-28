4d8ba22c5f7c38649ee36056e6a4c8b75a7885be
Keven Dubé <keven.andromediatech@gmail.com>
2019-01-28 13:13:29 -0500

    Bug fix about multiple ubidots-in node and auto reconnect after connection lost

--- ubidots/ubidots_in.js
+++ ubidots/ubidots_in.js
@@ -1,99 +1,74 @@
 module.exports = function (RED) {
   var mqtt = require('mqtt');
 
-  function getClient(self, endpoint_url, label_device, label_variable, token) {
-    self.status({ fill: "green", shape: "ring", text: "ubidots.connecting" });
+  const NODENAME = "ubidots_in";
 
-    if(this.client !== null && this.client !== undefined) {
-      this.client.end(true, function() {});
-    }
+  function getClient(node, endpoint_url, label_device, label_variable, token) {
+    node.status({ fill: "green", shape: "ring", text: "ubidots.connecting" });
 
-    var client = mqtt.connect('mqtt://' + endpoint_url, {username: token, password: ""});
-    this.client = client;
+    const client = mqtt.connect('mqtt://' + endpoint_url, { username: token, password: "" });
+    node.client = client;
 
-    client.on("error", function () {
-      client.end(true, function() {});
-      self.status({ fill: "red", shape: "ring", text: "ubidots.error_connecting" });
+    client.on('message', function (topic, message, packet) {
+      node.emit("input", { payload: JSON.parse(message.toString()) });
     });
 
-    client.on('close', function(){
-      client.end(true, function(){});
+    client.on("error", function (error) {
+      node.error(error);
+      node.status({ fill: "red", shape: "ring", text: "ubidots.error_connecting" });
     });
 
-    client.on("reconnect", function () {
-      var topic = "/v1.6/devices/" + label_device + "/" + label_variable;
-      var options = {};
-
-      self.status({ fill: "green", shape: "dot", text: "ubidots.connected" });
-      options[topic] = 1;
-
-      client.subscribe(options, function (err, granted) {
-        try {
-          client.on('message', function (topic, message, packet) {
-            self.emit("input", {payload: JSON.parse(message.toString())});
-          });
-        } catch (e) {
-          self.status({ fill: "red", shape: "ring", text: "ubidots.error_connecting" });
-        }
-      });
+    client.on('close', function () {
+      node.status({ fill: "red", shape: "ring", text: "ubidots.offline" });
     });
 
     client.on("connect", function () {
       var topic = "/v1.6/devices/" + label_device + "/" + label_variable;
       var options = {};
 
-      self.status({ fill: "green", shape: "dot", text: "ubidots.connected" });
+      node.status({ fill: "green", shape: "dot", text: "ubidots.connected" });
       options[topic] = 1;
 
       client.subscribe(options, function (err, granted) {
-        try {
-          client.on('message', function (topic, message, packet) {
-            self.emit("input", {payload: JSON.parse(message.toString())});
-          });
-        } catch (e) {
-          self.status({ fill: "red", shape: "ring", text: "ubidots.error_connecting" });
-        }
+        
       });
     });
   }
 
-  function UbidotsNode(n) {
-    RED.nodes.createNode(this, n);
-    var self = this;
+  function UbidotsNode(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
 
-    var endpoint_urls = {
+    const endpoint_urls = {
       business: 'industrial.api.ubidots.com',
       educational: 'things.ubidots.com'
     };
 
-    var label_device = n.device_label || n.label_device;
-    var label_variable = n.label_variable;
-    var endpoint_url = endpoint_urls[n.tier] || endpoint_urls['business'];
-    var token = n.token;
+    const label_device = config.device_label || config.label_device;
+    const label_variable = config.label_variable;
+    const endpoint_url = endpoint_urls[config.tier] || endpoint_urls['business'];
+    const token = config.token;
 
-    getClient(self, endpoint_url, label_device, label_variable, token);
+    getClient(node, endpoint_url, label_device, label_variable, token);
 
-    this.on("error", function () {
-      if(self.client !== null && self.client !== undefined) {
-        self.client.end(true, function(){});
-      }
-      self.status({ fill: "red", shape: "ring", text: "ubidots.error_connecting" });
+    node.on("error", function () {
+      node.status({ fill: "red", shape: "ring", text: "ubidots.error_connecting" });
     });
 
-    this.on("close", function(){
-      if(self.client !== null && self.client !== undefined) {
-        self.client.end(true, function(){});
+    node.on("close", function () {
+      if (node.client !== null && node.client !== undefined) {
+        node.client.end(true, function () { });
       }
     });
 
-    this.on("input", function (msg) {
+    node.on("input", function (msg) {
       try {
-        this.send(msg);
+        node.send(msg);
       } catch (err) {
-        this.error(err, msg);
+        node.error(err, msg);
       }
     });
   }
 
-  RED.nodes.registerType("ubidots_in", UbidotsNode);
+  RED.nodes.registerType(NODENAME, UbidotsNode);
 };
